0483577036e9dde9fc485e6b89ceffa639ec2816
Lucas José <66851451+lucas-jsvd@users.noreply.github.com>
2020-06-25 15:11:15 -0300

    Resolução dos desafios da aula 2.
    Resolução dos desafios da aula 2, pulo duplo e colisões.

--- scripts/jogo/heroi.js
+++ scripts/jogo/heroi.js
@@ -1,32 +1,42 @@
 class Heroi extends Personagem {
-  constructor(imagem, proporcao, num_colunas, num_linhas){
+  constructor(imagem, proporcao, num_colunas, num_linhas) {
     super(imagem, proporcao, num_colunas, num_linhas)
     this.vel_pulo = 0;
     this.alt_max_pulo = 0;
     this.no_chao = true;
     this.pulando = false;
+    this.quant_pulo_max = 2
+    this.quant_pulo_atual = 0
+    this.pos_pulo_y = 0
   }
-  
-  
-  define_pular(vel_pulo, alt_max_pulo){
+
+
+  define_pular(vel_pulo, alt_max_pulo) {
     this.vel_pulo = vel_pulo;
-    this.alt_max_pulo = windowHeight - (this.altura_pers_prop + alt_max_pulo);
+    this.alt_max_pulo = alt_max_pulo;
   }
-  
-  teste(){
-    if (this.no_chao){
+
+  verificar_pular() {
+    if (this.no_chao || this.quant_pulo_atual < this.quant_pulo_max) {
+      this.pos_pulo_y = this.pos_atual_y
       this.pulando = true;
       this.no_chao = false;
+      this.quant_pulo_atual++;
     }
   }
-  
-  pular(){
-    if (this.pulando){
+
+  pular() {
+    if (this.pulando) {
       this.pos_atual_y -= this.vel_pulo;
     }
-    if (this.pos_atual_y <= this.alt_max_pulo)
-    {
+    if (this.pos_atual_y <= this.pos_pulo_y - this.alt_max_pulo) {
+      print("oi")
       this.pulando = false;
     }
+  }
+
+  verifica_colisao(obj_inimigo, precisao) {
+    let colidiu = collideRectRect(this.pos_atual_x, this.pos_atual_y, this.largura_pers_prop * precisao, this.altura_pers_prop * precisao, obj_inimigo.pos_atual_x, obj_inimigo.pos_atual_y, obj_inimigo.largura_pers_prop * precisao, obj_inimigo.altura_pers_prop * precisao);
+    return colidiu;
   }
 }
--- sketch.js
+++ sketch.js
@@ -9,78 +9,83 @@ let ini_gotinha;
 let cenario;
 let cenario2;
 
-function preload(){
+function preload() {
   img_background = loadImage("imagens/cenario/floresta.png");
-  img_heroi= loadImage("imagens/personagem/correndo.png");
+  img_heroi = loadImage("imagens/personagem/correndo.png");
   img_ini_gotinha = loadImage("imagens/inimigos/gotinha.png");
-  som_fundo = loadSound('sons/trilha_jogo.mp3')
+  som_fundo = loadSound('sons/trilha_jogo.mp3');
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   frameRate(fr);
-  gravidade = windowHeight/35
-  cenario1 = new Cenario(img_background, posX=0);
-  cenario2 = new Cenario(img_background, posX=windowWidth);
-  print(windowWidth, windowHeight)
-  heroi = cria_heroi(img_heroi, 0.3, 4, 4, windowHeight/30, windowHeight/4)
-  ini_gotinha = cria_inimigo(img_ini_gotinha, 0.15, 4, 7, windowWidth, windowHeight, windowHeight/45)
-  som_fundo.loop()
+  gravidade = windowHeight / 35;
+  cenario1 = new Cenario(img_background, posX = 0);
+  cenario2 = new Cenario(img_background, posX = windowWidth);
+  print(windowWidth, windowHeight);
+  heroi = cria_heroi(img_heroi, 0.3, 4, 4, windowHeight / 30, windowHeight / 3);
+  ini_gotinha = cria_inimigo(img_ini_gotinha, 0.15, 4, 7, windowWidth, windowHeight, windowHeight / 45);
+  som_fundo.loop();
 }
 
-function keyPressed(){
-  if(key == "ArrowUp"){
-    heroi.teste();
+function keyPressed() {
+  if (key == "ArrowUp") {
+    heroi.verificar_pular();
   }
 }
 
 function draw() {
-  func_cenarios(cenario1)
-  func_cenarios(cenario2)
-  func_herois(heroi)
-  func_inimigos(ini_gotinha)
+  func_cenarios(cenario1);
+  func_cenarios(cenario2);
+  func_herois(heroi);
+  func_inimigos(ini_gotinha);
   aplica_gravidade(heroi);
+  let colidiu_gotinha = heroi.verifica_colisao(ini_gotinha, 0.60);
+  if (colidiu_gotinha) {
+    noLoop();
+  }
 }
 
 
-function cria_heroi(img_heroi,proporcao, num_colunas, num_linhas, vel_pulo, alt_max_pulo) {
+function cria_heroi(img_heroi, proporcao, num_colunas, num_linhas, vel_pulo, alt_max_pulo) {
   let obj_heroi = new Heroi(img_heroi, proporcao, num_colunas, num_linhas);
-  obj_heroi.define_pos_ini(pos_ini_x=0, pos_ini_y=windowHeight - obj_heroi.altura_pers_prop)
+  obj_heroi.define_pos_ini(pos_ini_x = 0, pos_ini_y = windowHeight - obj_heroi.altura_pers_prop)
   obj_heroi.define_pular(vel_pulo, alt_max_pulo)
   return obj_heroi
 }
 
 
 function cria_inimigo(imagem_inimigo, proporcao, num_colunas, num_linhas, pos_ini_x, pos_ini_y, velocidade) {
-  let obj_ini = new Inimigo(imagem=imagem_inimigo, proporcao=proporcao, num_colunas=num_colunas, num_linhas=num_linhas)
-  obj_ini.define_pos_ini(pos_ini_x=pos_ini_x - obj_ini.largura_pers_prop, pos_ini_y=pos_ini_y - obj_ini.altura_pers_prop)
+  let obj_ini = new Inimigo(imagem = imagem_inimigo, proporcao = proporcao, num_colunas = num_colunas, num_linhas = num_linhas)
+  obj_ini.define_pos_ini(pos_ini_x = pos_ini_x - obj_ini.largura_pers_prop, pos_ini_y = pos_ini_y - obj_ini.altura_pers_prop)
   obj_ini.define_velocidade(velocidade)
   return obj_ini
 }
 
 
-function aplica_gravidade(personagem){
-  if (personagem.pulando == false && personagem.no_chao == false){
-      personagem.pos_atual_y += gravidade;
-    }
-  if (personagem.pos_atual_y >= personagem.pos_ini_y){
+function aplica_gravidade(personagem) {
+  if (personagem.pulando == false && personagem.no_chao == false) {
+    personagem.pos_atual_y += gravidade;
+  }
+  if (personagem.pos_atual_y >= personagem.pos_ini_y) {
     personagem.pos_atual_y = personagem.pos_ini_y
     personagem.no_chao = true
+    personagem.quant_pulo_atual = 0
   }
 }
 
-function func_herois(obj_heroi){
+function func_herois(obj_heroi) {
   obj_heroi.exibe();
   obj_heroi.anim_spriter();
   obj_heroi.pular()
 }
 
-function func_inimigos(obj_inimigo){
+function func_inimigos(obj_inimigo) {
   obj_inimigo.exibe()
-  obj_inimigo.move();  
+  obj_inimigo.move();
 }
 
-function func_cenarios(obj_cenario){
+function func_cenarios(obj_cenario) {
   obj_cenario.exibe();
   obj_cenario.anim_spriter();
 }
